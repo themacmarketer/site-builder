abf2df617a03bde8c391f2a5ea8975883e7683a6
leveled-up <leveled-up@users.noreply.github.com>
2017-03-08 18:37:29 +0100

    Update core.php

--- core.php
+++ core.php
@@ -1,18 +1,5 @@
 <?php
 function echohead()
 {
-echo '
-<!DOCTYPE html><html lang="en"><head>  <title>RunStorage Site Builder</title>  <meta charset="utf-8">  <meta name="viewport" content="width=device-width, initial-scale=1">  <meta name="theme-color" content="#009900">  <link rel="shortcut icon" href="https://www.runstorageapis.com/favicon.ico" type="image/x-icon">  <link rel="stylesheet" href="https://www.runstorageapis.com/css/bootstrap.min.css">  <link rel="stylesheet" href="https://www.runstorageapis.com/css/bootstrap-theme.min.css">  <link rel="stylesheet" href="https://www.runstorageapis.com/css/cstyle.css">  <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>  <script src="https://www.runstorageapis.com/js/bootstrap.min.js"></script>
-<script src="//cdn.ckeditor.com/4.6.2/standard/ckeditor.js"></script>
-<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">  <script src="https://www.runstorageapis.com/js/bootstrap-notify.min.js"></script>  <style>body{font-family: \'Roboto\', sans-serif;}</style></head><body><div class="blog-masthead">
-      <div class="container">
-        <nav class="blog-nav">
-          <a class="blog-nav-item" href="https://www.runstorage.com/cloud/">RunStorage Cloud</a>
-          <a class="blog-nav-item active" href="/">Site Builder</a>
-          <a class="blog-nav-item" href="/openp">Open PKG-File</a>
-        </nav>
-      </div>
-    </div>
-<div class="container">
-<br>';
+echo file_get_contents('styles/main-ui.html');
 }
