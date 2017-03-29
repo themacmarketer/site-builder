61baad1362bd27c7d2be875544fc7c5400f84dfc
leveled-up <leveled-up@users.noreply.github.com>
2017-03-29 11:26:30 +0200

    Update download.php

--- download.php
+++ download.php
@@ -1,6 +1,8 @@
 <?php
-header("Content-Type: application/zip");
+// Site Builder
+// Download a package
 
+header("Content-Type: application/zip");
 header("Content-Disposition: attachment; filename=\"website-".time().".json.pkg\"");
 
 echo file_get_contents('https://lh5.runstorageapis.com/'.$_GET["id"]);
