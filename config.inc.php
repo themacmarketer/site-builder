e0d9e42a201aaeaafe9e5b7977674341aa04049f
leveled-up <leveled-up@users.noreply.github.com>
2017-04-29 15:52:39 +0200

    Create config.inc.php

---
+++ config.inc.php
@@ -0,0 +1,7 @@
+<?php
+// config.inc.php
+// Configuration of Site Builder
+
+// Templates
+$config["templates"]["base_directory"] = "templates";
+$config["templates"]["list_file"] = "templates.json";
