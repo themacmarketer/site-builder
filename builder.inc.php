b4b7a35213376288dd9f4d57f504627c35412a66
leveled-up <leveled-up@users.noreply.github.com>
2017-04-29 15:50:16 +0200

    Update builder.inc.php

--- builder.inc.php
+++ builder.inc.php
@@ -32,3 +32,17 @@ function insert_content_into_template ($template, $title, $description, $content
   return $template;
   
 }
+
+function load_template_from_file ($template_id) {
+    
+  global $config;
+  $template_list = json_decode(file_get_contents($config["templates"]["base_directory"]."/templates.json"), true);
+  $template = $config["templates"]["base_directory"].$template_list["templates"][$template_id][0];
+  
+  if(!file_exists($template)) return false;
+  $template_content = file_get_contents($template);
+  if($template_content === FALSE OR $template_content === "") return false;
+  
+  return $template_content;
+  
+}
