70ec2e4d5796fff379265d635b2ab500c5fb2fea
leveled-up <leveled-up@users.noreply.github.com>
2017-04-29 16:27:06 +0200

    Update compile.php

--- compile.php
+++ compile.php
@@ -5,4 +5,25 @@ include('config.inc.php');
 include('parser.inc.php');
 include('builder.inc.php');
 
-$list = get_template_list();
+$template_list = get_template_list();
+if(!$template_list) exit("Error: Template List Unavailable");
+
+$input["title"] = htmlentities($_POST["title"]);
+$input["title_raw"] = $_POST["title"];
+$input["description"] = htmlentities($_POST["description"]);
+$input["style"] = $_POST["style"];
+$input["content"] = file_get_contents($_FILES["userfile"]["tmp_name"]);
+unlink($_FILES["userfile"]["tmp_name"]);
+
+$template = load_template_from_file($input["style"]);
+if(!$template) exit("Error: Template '$input[style]' Unavailable");
+
+$html = markdown_to_html($input["content"]);
+$site = insert_content_into_template ($template, $input["title"], $input["description"], $html);
+
+$type = "text/html";
+$filename = preg_replace('/[^\da-z]/i', '', $input["title_raw"]).".html";
+header("Content-Type: $type");
+header("Content-Disposition: attachment; filename=\"$filename\"");
+
+echo $site;
