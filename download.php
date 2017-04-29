62e087c70f301809c150a5ab92c3e01f452ce031
leveled-up <leveled-up@users.noreply.github.com>
2017-04-29 15:14:29 +0200

    Delete download.php

--- download.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Site Builder
-// Download a package
-
-header("Content-Type: application/zip");
-header("Content-Disposition: attachment; filename=\"website-".time().".json.pkg\"");
-
-echo file_get_contents('https://lh5.runstorageapis.com/'.$_GET["id"]);
